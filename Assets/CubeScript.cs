37c809cb96e5c3310084f618588930b6521f4c18
TheStilk <serkebaevmadiyar08@gmail.com>
2025-10-07 00:11:34 +0500

    Game Development Practice 2

---
+++ Assets/CubeScript.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+   void Start()
+   {
+       Debug.Log("Hello, Unity 3D!");
+   }
+   
+   void Update()
+    {
+        
+    }
+}
